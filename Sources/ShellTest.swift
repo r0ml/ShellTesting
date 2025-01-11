836dd9769d174095df94145d2b8f63a03ed50c35
Robert Lefkowitz <r0ml@mac.com>
2025-01-10 22:18:48 -0500

    hunting bug in Matchable output

--- Sources/ShellTest.swift
+++ Sources/ShellTest.swift
@@ -325,6 +325,9 @@ public actor ShellProcess {
         case is Regex<Substring>:
           let jj = output as! Regex<Substring>
           #expect( j!.matches(of: jj).count > 0, Comment(rawValue: "\(j!) does not match expected output"))
+        case is Regex<AnyRegexOutput>:
+          let jj = output as! Regex<AnyRegexOutput>
+          #expect( j!.matches(of: jj).count > 0, Comment(rawValue: "\(j!) does not match expected output"))
         default:
           fatalError("not possible")
       }
